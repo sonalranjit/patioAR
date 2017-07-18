5624bb5a2927b71c60683d66c8a321bb160fd2b4
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-07-18 02:07:40 +0000

    model loading in aframe

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import 'aframe';
+import 'aframe-animation-component';
+import 'babel-polyfill';
+import {Entity, Scene} from 'aframe-react';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class AframePage extends React.Component{
+    render(){
+        return(
+            <div>
+             <Scene>
+              <a-assets>
+                <a-asset-item id="launchdemo-obj" src="models/LaunchDemo.obj"></a-asset-item>
+                <a-asset-item id="launchdemo-mtl" src="models/LaunchDemo.mtl"></a-asset-item>
+              </a-assets>
+              <a-entity obj-model="obj: #launchdemo-obj; mtl: #launchdemo-mtl;"
+              scale="0.04 0.04 0.04"
+              rotation="0 0 0"
+              position="-2 0 0">
+              </a-entity>
+            </Scene>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<AframePage />, document.getElementById('root'));
 registerServiceWorker();
