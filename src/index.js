a98a4883533c6b8f08a745e4301e7968d699b6dd
Sonal Ranjit <sonal.ranjit3@gmail.com>
2017-07-18 02:26:21 +0000

    TODO: fix third party ARjs module

--- src/index.js
+++ src/index.js
@@ -27,5 +27,29 @@ class AframePage extends React.Component{
     }
 }
 
-ReactDOM.render(<AframePage />, document.getElementById('root'));
+class AframeAR extends React.Component{
+    render(){
+        return(
+        <div>
+         <a-scene embedded arjs='sourceType: webcam;'>
+             <a-assets>
+              <a-asset-item id="launchdemo-obj" src="models/LaunchDemo.obj"></a-asset-item>
+              <a-asset-item id="launchdemo-mtl" src="models/LaunchDemo.mtl"></a-asset-item>
+             </a-assets>
+            <a-marker type='pattern' url='./patio-marker.patt' size='1'>
+              <a-entity obj-model="obj: #launchdemo-obj; mtl: #launchdemo-mtl"
+              scale="0.04 0.04 0.04"
+              rotation="0 0 0"
+              position="-2 0 0">
+              </a-entity>
+            </a-marker>
+            <a-entity camera></a-entity>
+         </a-scene>
+        </div>
+        );
+    }
+    
+}
+
+ReactDOM.render(<AframeAR />, document.getElementById('root'));
 registerServiceWorker();
